f12db9bf50e658d7339d2a743ce6d597ed18dad5
Apple <s10198298@connect.np.edu.sg>
2020-11-05 09:15:42 +0800

    Complete week 3

---
+++ Practical3/Practical3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practical3
+//
+//  Created by Apple on 4/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
